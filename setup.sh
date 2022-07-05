7b905ad7dda68baa281e13adeb1e8f2bd4aa47ef
akvo-fajro <96491243+akvo-fajro@users.noreply.github.com>
2022-07-05 11:22:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -48,31 +48,3 @@ cat ./data/accounts_pages/views.py > ./site/schoollunchweb/accounts_pages/views.
 mv ./data/accounts_pages/users_pages ./site/schoollunchweb/accounts_pages
 mv ./data/uwsgi.ini ./site/schoollunchweb
 cat ./data/other/extent_bashrc >> ~/.bashrc
-sleep 5
-source ~/.bashrc
-env | grep DJANGO
-source ~/.bashrc
-env | grep DJANGO
-rm ./data/other/extent_bashrc
-mv ./data/other/* ./site
-cd ./site/schoollunchweb
-python3 manage.py collectstatic
-cd ../
-
-# start the mysql in docker
-# work dir : ./site
-docker pull mysql
-docker-compose -f mysqldb_docker.yml up -d
-sleep 70
-
-# set django up and run the uwsgi
-# work dir : ./site
-python3 ./schoollunchweb/manage.py makemigrations
-python3 ./schoollunchweb/manage.py migrate
-python3 ./schoollunchweb/manage.py createsuperuser
-python3 ./schoollunchweb/manage.py shell < ./schoollunchweb_init.py
-python3 ./schoollunchweb/manage.py migrate
-cd schoollunchweb
-uwsgi -d --ini uwsgi.ini
-cd ..
-python3 timejob.py &
